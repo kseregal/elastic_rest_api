049684ff42943437268935647af7d6fd84521403
kseregal <skablov@gmail.com>
2017-12-21 17:14:48 +0300

    добавлен try catch

--- lib/src/elastic.dart
+++ lib/src/elastic.dart
@@ -21,37 +21,45 @@ class ElasticRequest {
   Future delete(String path) => _request('DELETE', path);
 
   Future _request(String method, String path, [body]) async {
-    var request = new http.Request(method, Uri.parse('$host/$path'));
+    var responseBody;
+    try {
+      var request = new http.Request(method, Uri.parse('$host/$path'));
 
-    if (body != null) {
-      if (body is! String) {
-        body = JSON.encode(body);
-      }
-      request.body = body;
-    }
-
-    var _headerStr = UTF8.encode("$_login:$_password");
-    var _base64HeaderStr = BASE64.encode(_headerStr);
-    request.headers['Authorization'] = "Basic $_base64HeaderStr";
-    //print(request.toString());
-
-    var response = await client.send(request);
-    var responseBody = _responseDecoder.convert(await response.stream.toBytes());
-    //print(responseBody.toString());
-    //print(response.statusCode);
-
-    if (response.statusCode >= 400) {
-      if (response.statusCode == 404) {
-        /// Записи не найдены.
-      } else {
-        var error = responseBody['error'];
-        if ((error is String && error.startsWith('IndexMissingException'))
-            || (error is Map && error['type'] == 'index_missing_exception')) {
-          throw new IndexMissingException(responseBody);
+      if (body != null) {
+        if (body is! String) {
+          body = JSON.encode(body);
         }
+        request.body = body;
       }
 
-      //throw new ElasticsearchException(responseBody);
+      var _headerStr = UTF8.encode("$_login:$_password");
+      var _base64HeaderStr = BASE64.encode(_headerStr);
+      request.headers['Authorization'] = "Basic $_base64HeaderStr";
+      //print(request.toString());
+
+      var response = await client.send(request);
+      responseBody = _responseDecoder.convert(
+          await response.stream.toBytes());
+      //print(responseBody.toString());
+      //print(response.statusCode);
+
+      if (response.statusCode >= 400) {
+        if (response.statusCode == 404) {
+          /// Записи не найдены.
+        } else {
+          var error = responseBody['error'];
+          if ((error is String && error.startsWith('IndexMissingException'))
+              || (error is Map && error['type'] == 'index_missing_exception')) {
+            throw new IndexMissingException(responseBody);
+          }
+        }
+
+        //throw new ElasticsearchException(responseBody);
+      }
+    } catch(e, stackTrace){
+      print(e.toString());
+      print(path.toString());
+      print(stackTrace);
     }
 
     return responseBody;
